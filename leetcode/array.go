42f2cd7250f0ef28c046827d902f9f32b412e973
xuwenjin01 <xuwenjin01@baidu.com>
2021-08-31 11:34:10 +0800

    update

--- leetcode/array.go
+++ leetcode/array.go
@@ -15,3 +15,37 @@ func pivotIndex(nums []int) int {
 	}
 	return -1
 }
+
+//搜索插入位置
+func searchInsert(nums []int, target int) int {
+	n := len(nums)
+	left, right := 0, n-1
+	ans := n
+	for left <= right {
+		mid := (right-left)>>1 + left
+		if target <= nums[mid] {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return ans
+}
+
+// 旋转矩阵
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	// 水平翻转
+	for i := 0; i < n/2; i++ {
+		matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+	}
+	// 主对角线翻转
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
+
+//
